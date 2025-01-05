8e9794b9ab4f93127cb984f5ab2477cc4fc13251
Prakhar <pawasthi961@gmail.com>
2025-01-05 11:06:16 +0530

    added terraform related files

---
+++ deployments/terraform/main.tf
@@ -0,0 +1,16 @@
+# main.tf
+provider "google" {
+  project     = var.gcp_project_id
+  region      = var.gcp_region
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "go-test-cluster"
+  location = var.gcp_region
+  initial_node_count = 1
+
+  node_config {
+    machine_type = "e2-micro"
+  }
+}
+
---
+++ deployments/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "gcp_project_id" {
+  type = string
+  description = "The ID of the GCP project"
+}
+
+variable "gcp_region" {
+  type = string
+  description = "The GCP region where resources will be created"
+}
+
+
